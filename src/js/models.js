d641b3fc810b1394ac5068277a8d8084e89c82e4
GraceOyiza <graceoyiza13@gmail.com>
2021-04-22 05:27:47 +0100

    Create class for todo

--- src/js/models.js
+++ src/js/models.js
@@ -7,3 +7,24 @@ export class Project {
     return JSON.parse(localStorage.getItem('projects')) || [];
   }
 }
+
+export class Todo {
+  constructor(props) {
+    this.title = props.title;
+    this.date = new Date(props.date);
+    this.description = props.description;
+    this.priority = props.priority;
+    this.project = props.project;
+  }
+
+  save() {
+    const allTodos = Todo.getAll();
+
+    allTodos.push(this);
+    localStorage.setItem('todos', JSON.stringify(allTodos));
+  }
+
+  static getAll() {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+  }
+}
