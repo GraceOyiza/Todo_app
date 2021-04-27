20c4ce69d6034db29646dc1485311ce7db6f5f62
GraceOyiza <graceoyiza13@gmail.com>
2021-04-27 12:48:34 +0100

    Project ID function generated

--- src/js/models.js
+++ src/js/models.js
@@ -8,6 +8,10 @@ export class Project {
     this.name = name;
   }
 
+  static get(id) {
+    return Project.getAll().find((project) => project.id === id);
+  }
+
   static getAll() {
     return JSON.parse(localStorage.getItem('projects')) || [];
   }
