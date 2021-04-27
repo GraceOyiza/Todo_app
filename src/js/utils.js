e6e205d3aee2a56a4fcc0709f2fb15d02cc95a68
GraceOyiza <graceoyiza13@gmail.com>
2021-04-27 09:36:29 +0100

    Create appendtodo function to append todo

---
+++ src/js/utils.js
@@ -0,0 +1,10 @@
+const parser = Range.prototype.createContextualFragment.bind(
+  document.createRange(),
+);
+
+export function appendTodo(todo) {
+  const rawHTML = makeTodoCard(todo);
+  const parsedHTML = parser(rawHTML, 'text/html');
+
+  document.getElementById('projectTodos').appendChild(parsedHTML);
+}
