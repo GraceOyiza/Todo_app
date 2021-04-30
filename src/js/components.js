ba222d0f2b4e379e38fc42322e44ab55909a93f3
GraceOyiza <graceoyiza13@gmail.com>
2021-04-30 21:58:15 +0100

    Fix onclick errors

--- src/js/components.js
+++ src/js/components.js
@@ -3,21 +3,27 @@ import { parser } from './utils';
 
 export function makeTodoCard(todo) {
   return ` 
-    <div class="card w-100">
+    <div class="card w-100 mb-3" data-todo-card="${todo.id}">
       <div class="card-body">
         <h5 class="card-title">${todo.title}</h5>
         <p class="card-text">${todo.description} </p>
         <ul class="list-group">
-          <li class="list-group-item">Date:  ${format(
+          <li class="list-group-item py-1">Date: ${format(
             todo.date,
             'dd-mm-yyyy',
           )}</li>
-          <li class="list-group-item">Priority: ${todo.priority}</li>         
+          <li class="list-group-item py-1">Priority: ${
+            todo.priority
+          }</li>         
         </ul>
       </div>
       <div class="card-footer">
-        <button class="btn"><i class="bi bi-trash"></i></button>
-        <button class="btn"><i class="bi bi-pencil-fill"></i></button>
+        <button class="btn py-0" data-delete="${
+          todo.id
+        }"><i class="bi bi-trash"></i></button>
+        <button class="btn py-0" data-update="${
+          todo.id
+        }"><i class="bi bi-pencil-fill"></i></button>
       </div>
     </div>`;
 }
